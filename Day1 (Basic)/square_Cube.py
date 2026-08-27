e554702ba3f1e4d0c3fc861fd49b6f5a709c16b2
Tarun Malik <malik1642007@gmail.com>
2026-08-27 21:42:04 +0530

    Square/cube

---
+++ Day1 (Basic)/square_Cube.py
@@ -0,0 +1,7 @@
+#program to input 2 numbers and print their square, square root and cube
+print("-----------------------------------------------")
+num=int(input("Enter a number : "))
+print("-----------------------------------------------")
+print("Square of ","num"," is ",num*num)
+print("Square Root of ","num"," is ",num**0.5)
+print("Cube of ","num"," is ",num*num*num)
