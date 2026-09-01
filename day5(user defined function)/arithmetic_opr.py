85140a6018de9b6525afc30a4d0c4d91498b1614
Tarun Malik <malik1642007@gmail.com>
2026-09-01 22:18:27 +0530

    Create arithmetic_opr.py

---
+++ day5(user defined function)/arithmetic_opr.py
@@ -0,0 +1,12 @@
+# WAP that receives two numbers in a function and returns the result of all arithmetic operations (+,-,*,/,%)
+def arCalc(x,y):
+    return x+y, x-y, x*y, x/y, x%y
+
+num1=int(input("Enter number1 : "))
+num2=int(input("Enter number2 : "))
+add, sub, mult, div, mod=arCalc(num1, num2)
+print("Sum of given numbers : ",add)
+print("Subtraction of given numbers : ",sub)
+print("Product of given numbers : ",mult)
+print("Division of given numbers : ",div)
+print("Modulo of given numbers : ",mod)
