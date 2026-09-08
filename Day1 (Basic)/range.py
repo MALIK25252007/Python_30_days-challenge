d8b5ddecd6be1cc837ddba4a66086946a15cbf6a
Tarun Malik <malik1642007@gmail.com>
2026-09-08 10:16:59 +0530

    remove duplicate files

--- Day1 (Basic)/range.py
+++
@@ -1,4 +0,0 @@
-# WAP to print cubes of numbers in range 15 to 20
-for i in range(15,21):
-    print("cube of number ",i,end='')
-    print(" is ",i**3)
--- day5(user defined function)/sun_using_function.py
+++
@@ -1,9 +0,0 @@
-#Program to add two numbers through a function
-def calcSum(x,y):
-    s=x+y
-    return s
-
-num1=float(input("Enter first number : "))
-num2=float(input("Enter second number : "))
-sum=calcSum(num1,num2)
-print("sum of these two numbers is : ",sum)
