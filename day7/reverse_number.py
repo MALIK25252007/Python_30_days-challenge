f3ac650e6fb7d848f6fa0ed6e4bd381cfcb8f9c4
Tarun Malik <malik1642007@gmail.com>
2026-09-08 10:18:09 +0530

    remove duplicate files

--- day7/reverse_number.py
+++
@@ -1,8 +0,0 @@
-#WAP to print the reverse of the givem number
-num=int(input("Enter a number : "))
-temp=0
-while(num):
-    temp*=10
-    temp+=num%10
-    num=num//10
-print("Number after reversing : ",temp)   
--- day8(file handling part-1)/printData.py
+++
@@ -1,9 +0,0 @@
-#Wap to read a text file line by line and display each word saperated by a #
-myFile=open("textfile.txt",'r')
-line=" "
-while line:
-    line=myFile.readline()
-    for word in line.split():
-        print(word,end='#')
-    print()
-myFile.close()
