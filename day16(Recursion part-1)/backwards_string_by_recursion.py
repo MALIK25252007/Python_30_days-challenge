ffbddb22de76837b1aa24a138b7be31a00b82b2c
Tarun Malik <malik1642007@gmail.com>
2026-09-15 00:24:18 +0530

    Day16 completed

---
+++ day16(Recursion part-1)/backwards_string_by_recursion.py
@@ -0,0 +1,10 @@
+# Write a recursive function to print a string backwards
+def bp(sg,n):
+    if n>0:
+        print(sg[n],end='')
+        bp(sg,n-1)
+    elif n==0:
+        print(sg[0])
+
+s=input("Enter a string : ")
+bp(s,len(s)-1)
